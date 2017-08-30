d9a74968ab2b44ad60cd1a4aae3687285824136c
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-08-30 11:03:33 +0200

    Removes console.log

--- api/handlers/shortcuts.js
+++ api/handlers/shortcuts.js
@@ -6,7 +6,6 @@ const Boom = require('boom')
 module.exports.shortcuts = (request, reply) => {
   const roles = request.query.roles.split(',').join('|')
   fetchUrl('https://api.ipify.org/?format=json').then(response => {
-    console.log(response)
     const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}&myIp=${response.ip}`
     fetchUrl(url).then((data) => {
       return reply(data)
