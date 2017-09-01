99bfdad8e55b4a73aa3ed7ab469a5d57092a0698
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-09-01 09:14:03 +0200

    Fixes ip-lookup

--- api/handlers/shortcuts.js
+++ api/handlers/shortcuts.js
@@ -2,17 +2,19 @@
 
 import {fetchUrl} from '../helpers/fetch'
 const Boom = require('boom')
+const logger = require('../../src/lib/logger')
+
+function getMyIp (info) {
+  return info.remoteAddress ? `&myIp=${info.remoteAddress}` : ''
+}
 
 module.exports.shortcuts = (request, reply) => {
   const roles = request.query.roles.split(',').join('|')
-  fetchUrl('https://api.ipify.org/?format=json').then(response => {
-    const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}&myIp=${response.ip}`
-    fetchUrl(url).then((data) => {
-      return reply(data)
-    }).catch((error) => {
-      return reply(Boom.internal(error))
-    })
-  }).catch(error => {
+  const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}${getMyIp(request.info)}`
+  logger('info', ['shortcuts', url])
+  fetchUrl(url).then((data) => {
+    return reply(data)
+  }).catch((error) => {
     return reply(Boom.internal(error))
   })
 }
