e7c55dd4f5841e772efbbb9fde0a8acf509a79d2
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-09-04 18:33:39 +0200

    More ip-filtering

--- api/handlers/shortcuts.js
+++ api/handlers/shortcuts.js
@@ -4,13 +4,14 @@ import {fetchUrl} from '../helpers/fetch'
 const Boom = require('boom')
 const logger = require('../../src/lib/logger')
 
-function getMyIp (info) {
-  return info.remoteAddress ? `&myIp=${info.remoteAddress}` : ''
+function getMyIp (request) {
+  const ip = request.headers['x-forwarded-for'] ? request.headers['x-forwarded-for'] : request.info.remoteAddress
+  return `&myIp=${ip}`
 }
 
 module.exports.shortcuts = (request, reply) => {
   const roles = request.query.roles.split(',').join('|')
-  const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}${getMyIp(request.info)}`
+  const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}${getMyIp(request)}`
   logger('info', ['shortcuts', url])
   fetchUrl(url).then((data) => {
     return reply(data)
