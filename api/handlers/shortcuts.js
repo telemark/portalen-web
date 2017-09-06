e967d6ca579e96abe22c96d1899bd9b0ed274f31
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-09-06 14:31:56 +0200

    ip-filter for shortcuts

--- api/handlers/shortcuts.js
+++ api/handlers/shortcuts.js
@@ -6,9 +6,8 @@ const logger = require('../../src/lib/logger')
 
 module.exports.shortcuts = (request, reply) => {
   const roles = request.query.roles.split(',').join('|')
-  const myIp = request.query.ip || null
-  const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}&myIp=${myIp}`
-  logger('info', ['shortcuts', JSON.stringify(request.headers)])
+  const myIp = request.query.ip ? `&myIp=${request.query.ip}` : ''
+  const url = `https://shortcuts.portalen.win/shortcuts?roles=${roles}${myIp}`
   logger('info', ['shortcuts', url])
   fetchUrl(url).then((data) => {
     return reply(data)
